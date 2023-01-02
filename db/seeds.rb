85391f3f943d354551d2a5b13fa2b7829a750f63
Raga <ragawilliam570@gmail.com>
2023-01-03 01:20:08 +0300

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+messages = Message.create([
+  { message: 'This is the first message' },
+  { message: 'This is the second message' },
+  { message: 'This is the third message' },
+  { message: 'This is the fourth message' },
+  { message: 'This is the fifth message' },
+])
